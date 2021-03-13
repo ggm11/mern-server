857a42c62021cd29def9fe22cae4d41e5b3bae0c
gabriel <gabrigm90.11@gmail.com>
2021-03-13 17:28:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// vendors
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
